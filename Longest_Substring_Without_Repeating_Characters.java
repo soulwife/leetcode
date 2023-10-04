207a9c69d22a6c806824f7a8da6630cdd20c9d60
Anastasiia <mysterycconstellation@gmail.com>
2023-10-04 13:31:07 -0400

    Update Longest_Substring_Without_Repeating_Characters.java

--- Longest_Substring_Without_Repeating_Characters.java
+++ Longest_Substring_Without_Repeating_Characters.java
@@ -1,30 +1,32 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         char[] initialStringSet = s.toCharArray();
-        String nonRepeatingStringSet = new String(), temporarySet = new String(), currentItem = new String();
-        int index = 0;
+        StringBuilder temporarySet = new StringBuilder();
+        String currentItem = new String(), nonRepeatingStringSet = new String(), substring = new String();
+        int index = 0, temporarySetlength = 0;
         if (s.isEmpty()) return 0;
         for (int i = 0; i< initialStringSet.length; i++) {
             currentItem = Character.toString(initialStringSet[i]);
-            if (temporarySet.indexOf(initialStringSet[i]) < 0) 
-                temporarySet = temporarySet.concat(currentItem);
+            if (temporarySet.indexOf(currentItem) < 0) 
+                temporarySet.append(currentItem);
             else {
-                if (!temporarySet.isEmpty()) {
-                    index = temporarySet.indexOf(initialStringSet[i])+1;
-                    if (index < temporarySet.length())
-                        temporarySet = temporarySet.substring(index, temporarySet.length());
-                    else 
-                        temporarySet = "";   
+                if (temporarySet.length() > 0) {
+                    index = temporarySet.indexOf(currentItem)+1;
+                    temporarySetlength = temporarySet.length();
+                    substring = temporarySet.substring(index, temporarySet.length());
+                    temporarySet.setLength(0); 
+                    if (index < temporarySetlength)
+                        temporarySet.append(substring);
                 }
-                if(nonRepeatingStringSet.compareTo(temporarySet) == 0)
+                if(nonRepeatingStringSet.compareTo(temporarySet.toString()) == 0)
                 nonRepeatingStringSet = "";
 
-                if (temporarySet.indexOf(initialStringSet[i]) < 0) {
-                    temporarySet = temporarySet.concat(currentItem);
+                if (temporarySet.indexOf(currentItem) < 0) {
+                    temporarySet.append(currentItem);
                 }
             }
             if (nonRepeatingStringSet.length() < temporarySet.length())
-                nonRepeatingStringSet = temporarySet;
+                nonRepeatingStringSet = temporarySet.toString();
         }
 
         return nonRepeatingStringSet.length() > 0 ? nonRepeatingStringSet.length() : 1;
